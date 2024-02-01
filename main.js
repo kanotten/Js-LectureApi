143e089d5a1bbc34646300068d1643679018ee9c
Sheikh <kenneth.sheikh@hotmail.com>
2024-02-01 12:59:58 +0100

    added button for pushing Api tests

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-g; // fetch("catfact.ninja/facts", function(response) => {
+// fetch("catfact.ninja/facts", function(response) => {
 //   console.log(response);
 // });
 // Use callbacks to fix process dilemmas
 //
 //
 
-doSomething();
-doSomethingExpr();
+// let doSomethingExpr = function () {
+//   console.log("something");
+// };
 
-let doSomethingExpr = function () {
-  console.log("something");
-};
+// function doSomething() {
+//   console.log("something");
+// }
 
-function doSomething() {
-  console.log("something");
-}
-
-const catFactsContainer = document.getElementById("catFactsContainer");
-
-fetch("https://catfact.ninja/facts")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error:", error));
+// fetch("https://catfact.ninja/facts")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("Error:", error));
 
 // why do we need API´s ? use same pre-built Api´s to build diffirent applications.
 
@@ -66,10 +61,37 @@ console.log(toDo1.name); // result = false, since the name of toDo1 is called fa
 
 // Display cat facts on the web page in the div
 
-fetch("https://catfact.ninja/facts")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    catFactsContainer.innerHTML = `<p>${data.data[0].fact}</p>`;
-  })
-  .catch((error) => console.error("Error:", error));
+document.addEventListener("DOMContentLoaded", function () {
+  const catFactsContainer = document.getElementById("catFactsContainer");
+  const sendDataButton = document.getElementById("sendDataButton");
+
+  // Event listener for the button click
+  sendDataButton.addEventListener("click", function () {
+    // Get the HTML content of the catFactsContainer div
+    const catFactsContent = catFactsContainer.innerHTML;
+
+    // Make a POST request to a server (replace with your server endpoint)
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Cat Facts",
+        body: catFactsContent,
+        userId: 1,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("Server response:", data))
+      .catch((error) => console.error("Error:", error));
+  });
+
+  fetch("https://catfact.ninja/facts")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      catFactsContainer.innerHTML = `<p>${data.data[0].fact}</p>`;
+    })
+    .catch((error) => console.error("Error:", error));
+});
